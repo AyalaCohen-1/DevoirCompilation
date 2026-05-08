b2f048268dfd83c6c1edc6eafc6da4018e176ff1
AyalaCohen-1 <ayala.cohen@etu.univ-amu.fr>
2026-05-08 10:34:48 +0000

    nfa.h def

--- src/nfa.h
+++ src/nfa.h
@@ -1,4 +1,20 @@
 #ifndef NFA_H
 #define NFA_H
+#include "ast.h"
+
+typedef struct etat {
+    int no;
+    int symbole;
+    struct Etat *out1, *out2;    
+} Etat;
+
+typedef struct Nfa{
+    NfaType type;
+    char value;           
+    struct Nfa *gauche;    
+    struct Nfa *droit;
+} Nfa;
+
+void graphe_nfa(Nfa *automate);
 
 #endif 
